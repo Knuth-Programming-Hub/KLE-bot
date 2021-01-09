3e3cb2ab82da73f9967846b9e1a89fbbc710adeb
Hemant Panwar <45938556+hemant2132@users.noreply.github.com>
2021-01-09 22:09:32 +0530

    Paste command (#19)
    * feat: Create paste command
    
    - created GitHub gist for the code
    
    * enhance: Delete original message
    
    - also added creation-time to embed
    
    * Minor change for some old code.
    
    Co-authored-by: Utkarsh <utkarshpandey924@gmail.com>

---
+++ commands/misc/paste.js
@@ -0,0 +1,68 @@
+const octokit = require("../../octokit");
+
+const codeObj = {
+  filename: "",
+  code: "",
+};
+
+const compute = (args) => {
+  const codePos = args.indexOf("\n");
+
+  if (codePos === -1) return false;
+
+  codeObj.filename = "";
+  for (
+    let i = codePos - 1;
+    args[i] !== " " && args.substring(0, i + 1) !== "!paste";
+    --i
+  )
+    codeObj.filename += args[i];
+
+  codeObj.filename = codeObj.filename.split("").reverse().join("");
+  codeObj.code = args.substring(codePos + 1);
+
+  return codeObj.code !== "";
+};
+
+module.exports = {
+  name: "!paste",
+  description: "Paste code and get a link for it",
+  usage: "```!paste\n\nFormat:\n!paste <filename>\n<code>```",
+  execute: async (message, recievedArgs) => {
+    const args = String(message.content.trim());
+
+    if (!compute(args)) {
+      message.channel.send(
+        "Wrong format! Make sure the code is not empty. Use !help paste to know about usage."
+      );
+      return;
+    }
+
+    await octokit.gists
+      .create({
+        public: false,
+        files: {
+          [codeObj.filename]: { content: codeObj.code },
+        },
+      })
+      .then((gist) => {
+        const time = new Date();
+        const successMessage = {
+          content: `<@${message.author.id}> the code is ready! 😀`,
+          embed: {
+            title: "Find it here!",
+            url: `${gist.data.html_url}`,
+            color: 4045991,
+            description: `Created at ${time.toString()}.`,
+          },
+        };
+        message.channel.send(successMessage);
+      })
+      .catch((err) => {
+        message.reply("There was some error. 🙁");
+        console.log(err);
+      });
+
+    message.delete();
+  },
+};
