7fe056f9b51715fdecf4813abdc1dd1246d99219
Hemant Panwar <45938556+hemant2132@users.noreply.github.com>
2021-03-02 18:47:40 +0530

    React to message to claim Member role  (#34)
    * Remove the captcha process
    
    * Fix: Compare document with null in userHandlers
    
    * Uninstall captchapng
    
    * Feat: Give Member role on reaction

--- bot.js
+++ bot.js
@@ -4,13 +4,14 @@ const Discord = require("discord.js");
 const getFiles = require("./getFiles");
 const remind = require("./remind");
 const verify = require("./verify");
-const user = require("./utils/usersHandlers");
-const { sendCaptcha } = require("./utils/captcha");
 const { handleIdentify } = require("./utils/TLE");
 const { hasRole } = require("./utils/guildMemberHandlers");
 const getPrefix = require("./utils/getCommandPrefix");
+const reactionHandler = require("./utils/reactionHandler");
 
-const bot = new Discord.Client();
+const bot = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
 bot.commands = new Discord.Collection();
 
 bot.on("guildMemberAdd", async (member) => {
@@ -32,21 +33,11 @@ bot.on("guildMemberAdd", async (member) => {
       .setDescription(
         `
 I am your friendly bot written in Javascript.
-**Check your DM to solve the captcha**.
 *If* you wish to be identified as a JIITian, please send ${prefix}verify in the #verify channel :D.
 `
       )
       .setFooter(`Use ${prefix}help command to know more about me.`);
     channel.send(welcomeEmbed);
-
-    const passed = await sendCaptcha(bot, member.user);
-    if (passed === false) return;
-
-    // adding the member to the "users" collection in DB
-    const exists = await user.existsInUsers(member.id);
-    if (exists === false) {
-      await user.add(member.id);
-    }
   } catch (error) {
     bot.channels.cache.get(process.env.ERROR_LOG_CHANNEL).send(error.stack);
   }
@@ -60,7 +51,9 @@ bot.on("ready", () => {
         bot.commands.set(command.name, command);
       }
     })
-    .catch((err) => console.log(err));
+    .catch((error) => {
+      bot.channels.cache.get(process.env.ERROR_LOG_CHANNEL).send(error.stack);
+    });
   console.log("The KLE bot is online!");
 });
 
@@ -123,11 +116,32 @@ bot.on("message", async (message) => {
   }
 });
 
+bot.on("messageReactionAdd", async (reaction, discordUser) => {
+  if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+
+  try {
+    await reactionHandler(bot, reaction, discordUser, true);
+  } catch (error) {
+    bot.channels.cache.get(process.env.ERROR_LOG_CHANNEL).send(error.stack);
+  }
+});
+
+bot.on("messageReactionRemove", async (reaction, discordUser) => {
+  if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+
+  try {
+    await reactionHandler(bot, reaction, discordUser, false);
+  } catch (error) {
+    bot.channels.cache.get(process.env.ERROR_LOG_CHANNEL).send(error.stack);
+  }
+});
+
 bot.login(process.env.BOT_TOKEN);
 
 // web server
 const http = require("http");
-const captchapng = require("captchapng");
 const server = http.createServer((req, res) => {
   res.writeHead(200);
   res.end("ok");
---
+++ utils/reactionHandler.js
@@ -0,0 +1,28 @@
+const { addRole, hasRole, removeRole } = require("./guildMemberHandlers");
+const user = require("./usersHandlers");
+
+const reactForMember = async (bot, discordUser, add) => {
+  if (discordUser.bot === true) return;
+
+  if ((await hasRole(bot, discordUser, "JIITian")) === true) return;
+
+  if (add === true) {
+    if ((await hasRole(bot, discordUser, "Member")) === true) return;
+    addRole(bot, discordUser, "Member");
+    const exists = await user.existsInUsers(discordUser.id);
+    if (exists === false) {
+      await user.add(discordUser.id);
+    }
+  } else {
+    if ((await hasRole(bot, discordUser, "Member")) === true)
+      removeRole(bot, discordUser, "Member");
+  }
+};
+
+const reactionHandler = async (bot, reaction, discordUser, add) => {
+  if (reaction.message.channel.id === process.env.RULES_CHANNEL_ID) {
+    await reactForMember(bot, discordUser, add);
+  }
+};
+
+module.exports = reactionHandler;
